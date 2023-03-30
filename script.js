6855c22ef4a9e6cd63e37653ca9f47c8ae519991
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2023-03-30 09:01:28 +0200

    mostly done

---
+++ script.js
@@ -0,0 +1,98 @@
+const ENDPOIT = "https://dog-api.kinduff.com/";
+const DOG_FACTS = "api/facts";
+let used_facts = [];
+let current_fact;
+
+$(document).ready(() => {
+    let liked = new Liked_facts();
+    let disliked = new Disliked_facts();
+    $("#show-liked").hide();
+    $("#show-disliked").hide();
+
+
+    $("#like").click((e) => {
+        $("#dog-fact-text").toggle("slow").text("");
+        liked.putFact(current_fact);
+    });
+    $("#dislike").click((e) => {
+        $("#dog-fact-text").toggle("slow").text("");
+        disliked.putFact(current_fact);
+    });
+    $("#get-new").click((e) => {
+        $("#dog-fact-text").toggle("slow").text("");
+        GenerateDogFact(ENDPOIT + DOG_FACTS, "GET");
+        $("#dog-fact-text").show();
+    });
+
+
+    console.log("test");
+    GenerateDogFact(ENDPOIT + DOG_FACTS, "GET");
+})
+
+
+
+class A_fact {
+    constructor(fact) {
+        this.fact = fact;
+    }
+}
+
+class Liked_facts {
+    constructor() {
+        this.liked_facts = []
+    }
+
+    putFact(fact) {
+        this.liked_facts.push();
+        console.log("liked a fact \"" + fact.fact+ "\"");
+    }
+}
+
+class Disliked_facts {
+    constructor() {
+        this.disliked_facts = []
+    }
+
+    putFact(fact) {
+        this.disliked_facts.push();
+        console.log("disliked a fact \"" + fact.fact+ "\"");
+    }
+}
+
+function GetData(url, method, testtime, data) {
+    $.ajax({
+        url: url,
+        type: method,
+        headers: {},
+        if(data) {
+            data: data;
+        },
+        success: function (data, status) { WriteFact(url, method, data, testtime); console.log(data); },
+        error: function (data, status) { console.log(data) }
+    });
+}
+
+
+function GenerateDogFact(url, method, testtimes) {
+    GetData(url, method, testtimes);
+}
+
+
+
+function WriteFact(url, method, data, testtimes) {
+    const fact = data.facts[0]
+    if (testtimes > 5) {
+        alert("No new facts");
+        return;
+    }
+    testtimes++;
+    console.log(testtimes);
+    used_facts.forEach(element => {
+        if (element == fact) {
+            GenerateDogFact(url, method, testtimes);
+        }
+    });
+    used_facts.push(fact);
+    current_fact = new A_fact(fact);
+    $("#dog-fact-text").html(fact);
+}
